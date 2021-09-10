afd7eefaf0b235b869bfb18b5860c9be300b822f
Hossein <hosseinbeigi26@gmail.com>
2021-09-10 11:57:11 +0430

    dagger-hilt & Api setup

--- app/src/main/java/com/hossein/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/hossein/currencyconverter/MainActivity.kt
@@ -2,10 +2,15 @@ package com.hossein.currencyconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.hossein.currencyconverter.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
